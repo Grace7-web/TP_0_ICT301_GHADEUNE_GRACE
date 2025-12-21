ddd867280dbcb1472a18563a4a752f486f68b47b
unknown <SMAB INFORMATIQUE@GraceIct.ictl2.com>
2025-12-21 19:33:36 +0100

    ISP - implemetation apres refactoring

---
+++ ISP/Workable.java
@@ -0,0 +1,28 @@
+package ict301.solid.isp;
+
+interface Workable {
+    void work();
+}
+
+interface Eatable extends Workable {
+    void eat();
+}
+
+class HumanWorker implements Eatable {
+    @Override
+    public void work() {
+        System.out.println("Les humains travaillent");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Les humains mangent");
+    }
+}
+
+class RobotWorker implements Workable {
+    @Override
+    public void work() {
+        System.out.println("Les Robots travaillent sans fatigue");
+    }
+}
