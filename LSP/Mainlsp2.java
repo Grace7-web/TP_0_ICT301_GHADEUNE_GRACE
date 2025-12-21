340c6ff2e688b6d2e41239acfd950df37b9ab5fc
unknown <SMAB INFORMATIQUE@GraceIct.ictl2.com>
2025-12-21 19:24:52 +0100

    LSP- implementation apres refactoring

---
+++ LSP/Mainlsp2.java
@@ -0,0 +1,10 @@
+package ict301.solid.lsp;
+
+public class Mainlsp2 {
+    public static void main(String[] args) {
+        Shape square = new Square(3);
+        Shape rectangle = new Rectangle(3, 4);
+        System.out.println("Square Area : " + square.getArea());
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+    }
+}
